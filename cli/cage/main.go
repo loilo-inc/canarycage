254b61e96241984c06cd63b76535001d2002aede
Yusuke Sakurai <kerokerokerop@gmail.com>
2018-11-28 16:59:13 +0900

    tag: 2.0.0-alpha5

--- cli/cage/main.go
+++ cli/cage/main.go
@@ -19,7 +19,7 @@ func main() {
 	}
 	app := cli.NewApp()
 	app.Name = "canarycage"
-	app.Version = "2.0.0-alpha4"
+	app.Version = "2.0.0-alpha5"
 	app.Description = "A gradual roll-out deployment tool for AWS ECS"
 	app.Commands = cli.Commands{
 		commands.RollOutCommand(),
