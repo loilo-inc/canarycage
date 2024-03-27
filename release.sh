0d93d9b3aac88e5ee2ff8c7b61b23b296af9af77
Takafumi SEKIGUCHI <takkyuuplayer@gmail.com>
2024-03-27 17:30:18 +0900

    bump: go@1.22, gha, module (#65)
    * bump versions
    
    * update gha
    
    * revert aws sdk go

--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-docker run --rm --privileged \
-    -v ${PWD}:/go/src/github.com/loilo-inc/canarycage \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -w /go/src/github.com/loilo-inc/canarycage \
-    -e GITHUB_TOKEN=${GITHUB_TOKEN} \
-    goreleaser/goreleaser release --rm-dist
