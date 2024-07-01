fca8f4aed0dcf92cbcc318cc8e4f1de4e09705e1
Yusuke Sakurai <kerokerokerop@gmail.com>
2024-07-01 17:24:44 +0900

    Update timeout_test.go

--- timeout/timeout_test.go
+++ timeout/timeout_test.go
@@ -15,17 +15,20 @@ func TestManager(t *testing.T) {
 		assert.Equal(t, time.Duration(10), man.TaskStopped())
 		assert.Equal(t, time.Duration(10), man.TaskHealthCheck())
 		assert.Equal(t, time.Duration(10), man.ServiceStable())
+		assert.Equal(t, time.Duration(10), man.TargetHealthCheck())
 	})
 	t.Run("with config", func(t *testing.T) {
 		man := timeout.NewManager(10, &timeout.Input{
-			TaskRunningWait:     1,
-			TaskStoppedWait:     2,
-			TaskHealthCheckWait: 3,
-			ServiceStableWait:   4,
+			TaskRunningWait:       1,
+			TaskStoppedWait:       2,
+			TaskHealthCheckWait:   3,
+			ServiceStableWait:     4,
+			TargetHealthCheckWait: 5,
 		})
 		assert.Equal(t, time.Duration(1), man.TaskRunning())
 		assert.Equal(t, time.Duration(2), man.TaskStopped())
 		assert.Equal(t, time.Duration(3), man.TaskHealthCheck())
 		assert.Equal(t, time.Duration(4), man.ServiceStable())
+		assert.Equal(t, time.Duration(5), man.TargetHealthCheck())
 	})
 }
